e36ac2ed073246d7345be4d35456250626974d49
nicolaujoao1 <ilolanicolau1999@gmail.com>
2023-07-29 14:20:19 +0100

    Alteracoes

---
+++ src/ALFASOFT.UI/Startup.cs
@@ -0,0 +1,56 @@
+﻿using ALFASOFT.Data.Data;
+using ALFASOFT.IoC;
+using Microsoft.EntityFrameworkCore;
+
+namespace ALFASOFT.UI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+            }
+           );
+            services.RegisterSevices();
+            services.RegisterRepositories();
+
+            services.AddControllersWithViews();
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
