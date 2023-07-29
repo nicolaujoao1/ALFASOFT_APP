a0a3cc6e62de2fd3af855dd022a21e077eab7ef3
nicolaujoao1 <ilolanicolau1999@gmail.com>
2023-07-29 13:54:36 +0100

    Implementing services

---
+++ src/ALFASOFT.Application/Services/ContactService.cs
@@ -0,0 +1,50 @@
+﻿using ALFASOFT.Application.DTOs;
+using ALFASOFT.Domain.Entities;
+using ALFASOFT.Domain.Interfaces;
+using AutoMapper;
+
+namespace ALFASOFT.Application.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _contactService;
+        private readonly IMapper _mapper;
+
+        public ContactService(IContactRepository contactService, IMapper mapper)
+        {
+            _contactService = contactService;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> CreateAsync(ContactDTO entity)
+        {
+            var contact = _mapper.Map<Contact>(entity);
+
+            return await _contactService.CreateAsync(contact);
+        }
+
+        public async Task<bool> DeleteAsync(ContactDTO entity)
+        {
+            var contact = _mapper.Map<Contact>(entity);
+
+            return await _contactService.CreateAsync(contact);
+        }
+
+        public async Task<IEnumerable<ContactDTO>> GetAllAsync()
+        {
+            return _mapper.Map<IEnumerable<ContactDTO>>(await _contactService.GetAllAsync());
+        }
+
+        public async Task<ContactDTO> GetByIdAsync(Guid Id)
+        {
+            return _mapper.Map<ContactDTO>(await _contactService.GetByIdAsync(Id));
+        }
+
+        public async Task<bool> UpdateAsync(ContactDTO entity)
+        {
+            var contact = _mapper.Map<Contact>(entity);
+
+            return await _contactService.CreateAsync(contact);
+        }
+    }
+}
---
+++ src/ALFASOFT.Application/Services/IContactService.cs
@@ -0,0 +1,12 @@
+﻿using ALFASOFT.Application.DTOs;
+
+namespace ALFASOFT.Application.Services;
+
+public interface IContactService
+{
+    Task<IEnumerable<ContactDTO>> GetAllAsync();
+    Task<ContactDTO> GetByIdAsync(Guid Id);
+    Task<bool> CreateAsync(ContactDTO entity);
+    Task<bool> UpdateAsync(ContactDTO entity);
+    Task<bool> DeleteAsync(ContactDTO entity);
+}
